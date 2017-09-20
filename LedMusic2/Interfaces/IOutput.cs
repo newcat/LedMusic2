ddd4352dbb4894e8d9d64af1043a0c8d3d3271b2
newcat <kungkung@hotmail.de>
2017-09-20 17:37:21 +0200

    Preparations for Output

---
+++ LedMusic2/Interfaces/IOutput.cs
@@ -0,0 +1,20 @@
+﻿using LedMusic2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LedMusic2.Interfaces
+{
+    interface IOutput
+    {
+
+        string Name { get; }
+        Type ViewType { get; }
+
+        void CalculationDone(LedColor[] calculationResult);
+
+
+    }
+}
---
+++ LedMusic2/Outputs/TcpOutput.cs
@@ -0,0 +1,19 @@
+﻿using LedMusic2.Interfaces;
+using LedMusic2.Models;
+using System;
+
+namespace LedMusic2.Outputs
+{
+    class TcpOutput : IOutput
+    {
+        public string Name => "TCP";
+
+        public Type ViewType => typeof(TcpOutputView);
+
+        public void CalculationDone(LedColor[] calculationResult)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
