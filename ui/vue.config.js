5db7678d022b290bd2cd869bee431402b86cc411
newcat <freddy.wagner@web.de>
2018-09-08 20:33:03 +0200

    Embedded fonts

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-    configureWebpack: {
-        loader: [
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
-                    }
-                }]
-            }
-        ]
+    chainWebpack: config => {
+        config.module
+        .rule("fonts")
+        .test(/\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/)
+        .use("fileloader")
+        .loader("file-loader")
+        .options({
+            name: "[name].[ext]",
+            outputPath: "fonts/"
+        });
     }
 }
